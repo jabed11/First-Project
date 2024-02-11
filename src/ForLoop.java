871a620bea83dc469dca5e0a2bc655e12427bcf3
Jabed <jabedhossain1200@gmail.com>
2024-02-12 00:09:48 +0600

    changes2.0

---
+++ src/ForLoop.java
@@ -0,0 +1,22 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        
+        // for loop = executes a block of code for a limited amount of times
+
+
+        // for(int i=10; i>=0; i--) {
+        //     System.out.println(i);
+        // }
+        
+        // System.out.println("Happy New Year!");
+
+
+        for(int i=10; i>=0; i-=2) {
+            System.out.println(i);
+        }
+        
+        System.out.println("Happy New Year!");
+
+    }
+    
+}
---
+++ src/NestedLoop.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NestedLoop {
+    public static void main(String[] args) {
+        // nested loop = a loop inside of a loop
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int rows, columns;
+        String symbol;
+
+        System.out.print("Enter the number of rows: ");
+        rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        columns = scanner.nextInt();
+
+        System.out.print("Enter symbol to use: ");
+        symbol = scanner.next();
+
+        for(int i = 1; i<=rows; i++) {
+            System.out.println();
+            for(int j = 1; j<=columns; j++) {
+                System.out.print(symbol);
+            }
+        }
+    }
+   
+}
