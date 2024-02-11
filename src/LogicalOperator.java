5f0805774b828d838d83894f895e90fd67d81d35
Jabed <jabedhossain1200@gmail.com>
2024-02-11 18:30:37 +0600

    Changes

--- src/LogicalOperator.java
+++ src/LogicalOperator.java
@@ -22,23 +22,34 @@ public class LogicalOperator {
 
 
 
+        // Scanner sc = new Scanner(System.in);
+
+        // System.out.println("You are playing a game! Press q or Q to quit");
+        // String response = sc.next();
+
+        // if(response.equals("q") || response.equals("Q")) {
+        //     System.out.println("You quit the game!");
+        // }
+        // else {
+        //     System.out.println("You are still playing the game *pew pew*");
+        // }
+
+
+
+
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("You are playing a game! Press q or Q to quit");
         String response = sc.next();
 
-        if(response.equals("q") || response.equals("Q")) {
-            System.out.println("You quit the game!");
-        }
-        else {
+        if(!response.equals("q") && !response.equals("Q")) {
             System.out.println("You are still playing the game *pew pew*");
         }
-
-
-
-
-
-
+        else {
+            
+            System.out.println("You quit the game!");
+        }
 
 
 
