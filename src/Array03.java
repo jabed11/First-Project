51f31f698def3ec4c5b1e5c8dfab517e8d8b5066
Jabed <jabedhossain1200@gmail.com>
2024-02-21 15:19:38 +0600

    changes06

--- src/Array03.java
+++ src/Array03.java
@@ -1,6 +1,6 @@
 public class Array03 {
     public static void main(String[] args) {
-        int array[] = {10,20,30,40,50,60,70,80,90,100};
+        int arr[] = {10,20,30,40,50,60,70,80,90,100};
 
         // System.out.println("First element: "+array[0]);
         // System.out.println("Third element: "+array[2]);
@@ -8,7 +8,7 @@ public class Array03 {
 
         int i;
         for(i=0; i<10; i++) {
-            System.out.println(i+"th element is: "+array[i]);
+            System.out.println(i+"th element is: "+arr[i]);
         }
 
 
--- src/Functions.java
+++ src/Functions.java
@@ -50,20 +50,99 @@ public class Functions {
     
 
 ////    Multiplication of two numbers   
-        public static int calculateProduct(int a, int b) {
-            return a * b;
-        }
+
+        // public static int calculateProduct(int a, int b) {
+        //     return a * b;
+        // }
         
-        public static void main(String args[]) {
-            Scanner sc = new Scanner(System.in);
+        // public static void main(String args[]) {
+        //     Scanner sc = new Scanner(System.in);
 
-            System.out.println("Enter the first number: ");
-            int a = sc.nextInt();
-            System.out.println("Enter the second number: ");
-            int b = sc.nextInt();
+        //     System.out.println("Enter the first number: ");
+        //     int a = sc.nextInt();
+        //     System.out.println("Enter the second number: ");
+        //     int b = sc.nextInt();
 
-            System.out.println("The product of two numbers is: "+calculateProduct(a, b));
+        //     System.out.println("The product of two numbers is: "+calculateProduct(a, b));
             
+        // }
+
+
+
+        ////    Evaluate the factorial of a number    
+
+        // public static int printFactorial(int n) {
+            
+        //     if(n < 0) {
+        //         System.out.println("Invalid Number");
+        //         return n;
+        //     }
+
+        //     int factorial = 1;
+        //     for(int i=n; i>=1; i--) {
+        //         factorial = factorial * i;
+        //     }
+        //     System.out.print("The factorial of "+n+" is: ");
+        //     System.out.println(factorial);
+        //     return n;
+        // }
+        
+        // public static void main(String args[]) {
+
+        //     Scanner sc = new Scanner(System.in);
+        //     System.out.print("Enter a number: ");
+        //     int n = sc.nextInt();
+
+        //     printFactorial(n);
+                       
+        // }
+
+
+        //// Homework Problems 
+        //? Make a function to check if a number is prime or not.
+        //? Make a function to check if a given number n is even or not.
+        //? Make a function to print the table of a given number n.
+        //? Read about Recursion.
+
+
+
+
+        public static int printPrimeNumber(int n) {
+
+            boolean prime = true;
+            for(int i=2; i<n; i++) {
+                if (n % i == 0) {
+                    System.out.println("non prime");
+                    break;       
+                }
+            }
+            System.out.println(prime);
+        return n;
+    }
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            int n = sc.nextInt();
+
+            printPrimeNumber(n);
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
