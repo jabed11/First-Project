45cdde269e5b01a538c7198fc9a9de077a3cefd6
Jabed <jabedhossain1200@gmail.com>
2024-02-25 23:16:32 +0600

    changes08

--- src/Functions.java
+++ src/Functions.java
@@ -25,7 +25,7 @@ public class Functions {
 // }
 
         
-    ////    Addition of two numbers    
+////    Addition of two numbers    
 
     //     public static int calculateSum(int a, int b) {
     //         int sum = a + b;
@@ -49,7 +49,7 @@ public class Functions {
 
     
 
-////    Multiplication of two numbers   
+    ////    Multiplication of two numbers   
 
         // public static int calculateProduct(int a, int b) {
         //     return a * b;
@@ -66,7 +66,6 @@ public class Functions {
         //     System.out.println("The product of two numbers is: "+calculateProduct(a, b));
             
         // }
-
 
 
         ////    Evaluate the factorial of a number    
@@ -105,35 +104,120 @@ public class Functions {
         //? Read about Recursion.
 
 
+    ////    A number is prime or not    
+
+    //     public static int printPrimeNumber(int n) {
+
+    //         for(int i=2; i<n; i++) {
+    //             if (n % i == 0) {
+    //                 System.out.println("non prime");
+    //                 break;       
+    //             }
+    //         }
+    //         System.out.println("prime");
+    //     return n;
+    // }
+
+    //     public static void main(String[] args) {
+    //         Scanner sc = new Scanner(System.in);
+    //         System.out.println("Enter a number: ");
+    //         int n = sc.nextInt();
+
+    //         printPrimeNumber(n);
+    //     }
 
 
-        public static int printPrimeNumber(int n) {
+////    A given number n is even or not    
+// public static int evenOROdd(int n) {
 
-            boolean prime = true;
-            for(int i=2; i<n; i++) {
-                if (n % i == 0) {
-                    System.out.println("non prime");
-                    break;       
-                }
-            }
-            System.out.println(prime);
-        return n;
-    }
+//     if(n % 2 == 0) {
+//         System.out.println("Even");
+//     } else{
+//         System.out.println("Odd");
+//     }
+//     return n;
+// }
 
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number: ");
+//         int n = sc.nextInt();
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter a number: ");
-            int n = sc.nextInt();
-
-            printPrimeNumber(n);
-        }
+//         evenOROdd(n);
+//         }
 
 
+////    print the table of a given number n    
+
+// public static int printTable(int n) {
+
+//     for(int i=1; i<=10; i++){
+//         System.out.println(n+" X "+i+" = "+n*i);
+//     }
+//     return n;
+// }
 
 
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter a number: ");
+//     int n = sc.nextInt();
+
+//     printTable(n);
+// }
+
+////    Recursion    
+// public static void main(String[] args) {
+//     sayHi(3);
+// }
+
+// public static void sayHi(int count) {
+//     System.out.println("Hi");
+
+//     if (count <= 1) {
+//         return;
+//     }
+//     sayHi(count - 1);
+// }
 
 
+////  Questions  
+//? 1. Enter 3 numbers from the user & make a function to print their average.
+//? 2. Write a function to print the sum of all odd numbers from 1 to n.
+//? 3. Write a function which takes in 2 numbers and returns the greater of those two.
+//? 4. Write a function that takes in the radius as input and returns the circumference of a circle.
+//? 5. Write a function that takes in age as input and returns if that person is eligible to vote or not. A person of age > 18 is eligible to vote.
+//? 6. Write an infinite loop using do while condition.
+//? 7. Write a program to enter the numbers till the user wants and at the end it should display the count of positive, negative and zeros entered. 
+//? 8. Two numbers are entered by the user, x and n. Write a function to find the value of one number raised to the power of another i.e. xn.
+//? 9. Write a function that calculates the Greatest Common Divisor of 2 numbers. (BONUS)
+//? 10. Write a program to print Fibonacci series of n terms where n is input by user :
+//? 0 1 1 2 3 5 8 13 21 ..... 
+//? In the Fibonacci series, a number is the sum of the previous 2 numbers that came before it.
+//? (BONUS)
+
+
+////   1. Average    
+
+public static int ptintAverage(int num1, int num2, int num3){
+
+    int num1, num2, num3;
+    int average = num1+num2+num3;
+
+
+    return average;
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the first number: ");
+    int num1 = sc.nextInt();
+    System.out.println("Enter the second num1: ");
+    int num2 = sc.nextInt();
+    System.out.println("Enter the third num1: ");
+    int num3 = sc.nextInt();
+
+    printAverage();
+}
 
 
 
